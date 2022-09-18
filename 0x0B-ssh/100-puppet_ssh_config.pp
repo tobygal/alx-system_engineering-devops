b0a2e4c6d89d9a2a85776e5f131a020100a6d49b
tobygal <tochiazolike@yahoo.com>
2022-09-18 15:37:45 -0700

    Add config manifest

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#Manifest to set up SSH config file to auth via private key
+include stdlib
+file_line { 'Declare identity file':
+  path     => '/etc/ssh/ssh_config',
+  line     => '    IdentityFile ~/.ssh/school',
+  replace  => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path     => '/etc/ssh/ssh_config',
+  line     => '    PasswordAuthentication no',
+  replace  => true,
+}
