9d73489e83a6762415a7c47f8dd26a22a02a9e57
|Tochi Azolike <tochiazolike@yahoo.com>
2022-09-27 16:29:32 -0700

    update file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,3 +1,8 @@
+# add stable version of nginx
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
 # update software packages list
 exec { 'update packages':
   command => 'apt-get update',
